4d651372df70d7585e5c1aa60fd93ed69c789c11
GUMBAUElric <elric.gumbau@ynov.com>
2020-01-25 15:34:42 +0100

    add .circleci

--- .circleci/api/server.js
+++ .circleci/api/server.js
@@ -10,12 +10,6 @@ var server = express();
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
 
-// Configure routes
-server.get('/', function (req,res) {
-    res.setHeader('Content-Type','text/html');
-    res.status(200).send('<h1>Hello World !</h1>')
-});
-
 server.use('/api/', apiRouter);
 
 // Launch server
